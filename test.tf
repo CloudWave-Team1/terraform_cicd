25a96ee8d430843ce8bf9a1129704b96d6f1acd4
malangtofu <112858761+malangtofu@users.noreply.github.com>
2023-08-14 23:58:42 +0900

    test commit

--- test.tf
+++ test.tf
@@ -72,7 +72,7 @@ resource "aws_launch_template" "TFC_EC2_template" {
   image_id      = "ami-055179a7fc9fb032d"
   instance_type = "t2.micro"
   name_prefix   = "TFC-EC2-template"
-  user_data = file("./test.sh")
+  user_data = base64encode(file("./test.sh")) # 테스트용
 
   # 여기에 보안 그룹 지정
   vpc_security_group_ids = [aws_security_group.TFC_PRD_EC2_SG.id]
