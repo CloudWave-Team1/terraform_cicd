36ffb388e5134515203cd3519f2d0f0fcd8f5cb6
malangtofu <112858761+malangtofu@users.noreply.github.com>
2023-08-15 02:24:58 +0900

    test commit

--- test.tf
+++ test.tf
@@ -1,19 +1,5 @@
-# AWS 인증 변수 설정
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS Access Key"
-  type        = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS Secret Key"
-  type        = string
-}
-
-# AWS 프로바이더 설정
 provider "aws" {
-  region  = "ap-northeast-2"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region = "ap-northeast-2"
 }
 
 # VPC 생성
@@ -43,64 +29,91 @@ resource "aws_subnet" "TFC_PRD_sub" {
   }
 }
 
-# 인터넷 게이트웨이 생성
-resource "aws_internet_gateway" "TFC_PRD_IG" {
-  vpc_id = aws_vpc.TFC_PRD_VPC.id
-  tags = {
-    Name = "TFC-PRD-IG"
-  }
-}
-
-# 공개 IP 주소(EIP) 생성
-resource "aws_eip" "example" {
+# NAT Gateway 생성
+resource "aws_eip" "NAT_EIP" {
   count = 2
 }
 
-# NAT 게이트웨이 생성
 resource "aws_nat_gateway" "TFC_PRD_NG" {
   count         = 2
-  allocation_id = aws_eip.example[count.index].id
   subnet_id     = aws_subnet.TFC_PRD_sub[count.index].id
-  tags = {
-    Name = ["TFC-PRD-NG01", "TFC-PRD-NG02"][count.index]
-  }
+  allocation_id = aws_eip.NAT_EIP[count.index].id
 }
 
-# EC2 인스턴스 생성을 위한 템플릿
-resource "aws_launch_template" "TFC_EC2_template" {
-  description   = "TFC EC2 basic start"
-  image_id      = "ami-055179a7fc9fb032d"
-  instance_type = "t2.micro"
-  name_prefix   = "TFC-EC2-template"
-  user_data = base64encode(file("./userdata.sh"))
-
-  vpc_security_group_ids = [aws_security_group.TFC_PRD_EC2_SG.id]
-}
-
-# EC2 보안 그룹 생성
-resource "aws_security_group" "TFC_PRD_EC2_SG" {
+# Security Groups 생성
+resource "aws_security_group" "TFC_PRD_ELB_SG" {
   vpc_id = aws_vpc.TFC_PRD_VPC.id
-  
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.TFC_PRD_VPC.cidr_block]
-  }
-  
+
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/24"]
+  }
+
+  tags = {
+    Name = "TFC-PRD-ELB-SG"
+  }
+}
+
+resource "aws_security_group" "TFC_PRD_EC2_SG" {
+  vpc_id = aws_vpc.TFC_PRD_VPC.id
+
+  # 여기에 인바운드 규칙 추가
+
   tags = {
     Name = "TFC-PRD-EC2-SG"
   }
 }
 
-# Target Group 생성
+# Launch Template 생성
+resource "aws_launch_template" "TFC_EC2_template" {
+  name_prefix   = "TFC-EC2-template"
+  description   = "TFC EC2 basic start"
+  image_id      = "ami-055179a7fc9fb032d"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOT
+              #!/bin/bash
+              yum install -y httpd
+              echo "Hello, TFC" > /var/www/html/index.html
+              systemctl start httpd
+              systemctl enable httpd
+              EOT
+
+  vpc_security_group_ids = [aws_security_group.TFC_PRD_EC2_SG.id]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "TFC EC2 Instance"
+    }
+  }
+}
+
+# Application Load Balancer (ALB) 설정
+resource "aws_lb" "TFC_PRD_ELB" {
+  name               = "TFC-PRD-ELB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.TFC_PRD_ELB_SG.id]
+  subnets            = [aws_subnet.TFC_PRD_sub[2].id, aws_subnet.TFC_PRD_sub[3].id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "TFC-PRD-ELB"
+  }
+}
+
+# Target Group 설정
 resource "aws_lb_target_group" "TFC_PRD_TG" {
   name     = "TFC-PRD-TG"
   port     = 80
@@ -116,29 +129,12 @@ resource "aws_lb_target_group" "TFC_PRD_TG" {
     healthy_threshold   = 3
     unhealthy_threshold = 3
   }
-
-  tags = {
-    Name = "TFC-PRD-TG"
-  }
 }
 
-# Application Load Balancer 생성
-resource "aws_lb" "TFC_PRD_ELB" {
-  internal           = false
-  load_balancer_type = "application"
-  name               = "TFC-PRD-ELB"
-  security_groups    = [aws_security_group.TFC_PRD_ELB_SG.id]
-  subnets            = [aws_subnet.TFC_PRD_sub[0].id, aws_subnet.TFC_PRD_sub[1].id]
-  enable_deletion_protection = false
-  tags = {
-    Name = "TFC-PRD-ELB"
-  }
-}
-
-# ALB 리스너에서 대상 그룹을 default action으로 설정
+# Listener 설정
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.TFC_PRD_ELB.arn
-  port              = "80"
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
@@ -147,93 +143,38 @@ resource "aws_lb_listener" "front_end" {
   }
 }
 
-resource "aws_autoscaling_group" "TFC_PRD_ASG" {
+# Auto Scaling Group 설정
+resource "aws_autoscaling_group" "TFC_PRD_ASGP" {
+  desired_capacity     = 1
+  max_size             = 2
+  min_size             = 1
   launch_template {
     id      = aws_launch_template.TFC_EC2_template.id
     version = "$Latest"
   }
-
-  min_size             = 2
-  max_size             = 10
-  desired_capacity     = 4
-  vpc_zone_identifier  = [aws_subnet.TFC_PRD_sub[2].id, aws_subnet.TFC_PRD_sub[3].id, aws_subnet.TFC_PRD_sub[4].id, aws_subnet.TFC_PRD_sub[5].id]
-  
+  vpc_zone_identifier  = [aws_subnet.TFC_PRD_sub[2].id, aws_subnet.TFC_PRD_sub[3].id]
   target_group_arns    = [aws_lb_target_group.TFC_PRD_TG.arn]
 
-  tag {
-    key                 = "Name"
-    value               = "TFC-PRD-ASG"
-    propagate_at_launch = true
+  tags = [
+    {
+      key                 = "Name"
+      value               = "TFC-ASG-Instance"
+      propagate_at_launch = true
+    }
+  ]
+}
+
+# Scaling Policy
+resource "aws_autoscaling_policy" "TFC_PRD_ASGP_TTP" {
+  name                   = "TFC-PRD-ASGP-TTP"
+  autoscaling_group_name = aws_autoscaling_group.TFC_PRD_ASGP.name
+  policy_type            = "TargetTrackingScaling"
+  estimated_warmup       = 300
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 50.0
   }
-}
-
-# ALB 보안 그룹 생성
-resource "aws_security_group" "TFC_PRD_ELB_SG" {
-  vpc_id = aws_vpc.TFC_PRD_VPC.id
-  
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  tags = {
-    Name = "TFC-PRD-ELB-SG"
-  }
-}
-
-# 퍼블릿 서브넷 라우트 테이블 생성 및 연결
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.TFC_PRD_VPC.id
-}
-
-resource "aws_route" "public_route" {
-  route_table_id         = aws_route_table.public_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.TFC_PRD_IG.id
-}
-
-resource "aws_route_table_association" "public_a" {
-  subnet_id      = aws_subnet.TFC_PRD_sub[0].id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-resource "aws_route_table_association" "public_b" {
-  subnet_id      = aws_subnet.TFC_PRD_sub[1].id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-# 프라이빗 서브넷 라우트 테이블 생성 및 연결
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.TFC_PRD_VPC.id
-}
-
-resource "aws_route" "private_route_a" {
-  route_table_id         = aws_route_table.private_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.TFC_PRD_NG[0].id
-}
-
-resource "aws_route" "private_route_b" {
-  route_table_id         = aws_route_table.private_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.TFC_PRD_NG[1].id
-}
-
-resource "aws_route_table_association" "private_a" {
-  subnet_id      = aws_subnet.TFC_PRD_sub[2].id
-  route_table_id = aws_route_table.private_route_table.id
-}
-
-resource "aws_route_table_association" "private_b" {
-  subnet_id      = aws_subnet.TFC_PRD_sub[3].id
-  route_table_id = aws_route_table.private_route_table.id
 }
