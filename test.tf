0aef5627d26261d037ccb4fa39c2f8dc46b4a249
malangtofu <112858761+malangtofu@users.noreply.github.com>
2023-08-15 02:42:26 +0900

    test commit

--- test.tf
+++ test.tf
@@ -100,6 +100,28 @@ resource "aws_lb" "TFC_PRD_ELB" {
   }
 }
 
+resource "aws_security_group" "TFC_PRD_ELB_SG" {
+  vpc_id = aws_vpc.TFC_PRD_VPC.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "TFC-PRD-ELB-SG"
+  }
+}
+
 # Target Group 설정
 resource "aws_lb_target_group" "TFC_PRD_TG" {
   name     = "TFC-PRD-TG"
