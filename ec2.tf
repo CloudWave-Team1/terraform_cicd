1c63f55958ea81b65209100a6fd2f67aba9c9c61
malangtofu <112858761+malangtofu@users.noreply.github.com>
2023-08-16 11:05:33 +0900

    ec2  commit

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 # EC2 인스턴스 생성을 위한 템플릿
 resource "aws_launch_template" "TFC_EC2_template" {
   description   = "TFC EC2 basic start"
-  image_id      = "ami-0f2ce0bfb34039f29"
+  image_id      = "ami-03ba98da05afd63c6"
   instance_type = "t2.micro"
   name_prefix   = "TFC-EC2-template"
   # user_data = base64encode(file("./userdata.sh")) # 테스트 용
