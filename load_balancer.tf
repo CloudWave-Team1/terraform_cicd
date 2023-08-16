1e6e17d4c4d0df16e50181649f622c067e128ba0
malangtofu <112858761+malangtofu@users.noreply.github.com>
2023-08-16 14:37:26 +0900

    로드밸런스  commit

--- load_balancer.tf
+++ load_balancer.tf
@@ -44,3 +44,52 @@ resource "aws_lb_listener" "TFC_PRD_Listener" {
     target_group_arn = aws_lb_target_group.TFC_PRD_TG.arn
   }
 }
+
+# ========================================
+
+# ACM에서 인증서 생성
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "www.aws.devnote.dev"
+  validation_method = "DNS"
+}
+
+# 인증서 검증용 Route53 레코드 생성
+resource "aws_route53_record" "cert_validation" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
+  ttl     = 60
+}
+
+# 인증서 검증
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+}
+
+# 보안 그룹 수정: HTTPS 트래픽 허용
+resource "aws_security_group" "TFC_PRD_ELB_SG" {
+  # (이전 코드에서의 나머지 설정 유지)
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Application Load Balancer 수정: HTTPS 리스너 추가
+resource "aws_lb_listener" "TFC_PRD_Listener_HTTPS" {
+  load_balancer_arn = aws_lb.TFC_PRD_ELB.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08" # 보안 정책 설정
+  certificate_arn   = aws_acm_certificate_validation.cert.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.TFC_PRD_TG.arn
+  }
+}
