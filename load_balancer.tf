aaf2bc597253d6d158e848eb31bf12700f4f690b
malangtofu <112858761+malangtofu@users.noreply.github.com>
2023-08-16 14:44:40 +0900

    로드밸런스  commit

--- load_balancer.tf
+++ load_balancer.tf
@@ -56,9 +56,9 @@ resource "aws_acm_certificate" "cert" {
 # 인증서 검증용 Route53 레코드 생성
 resource "aws_route53_record" "cert_validation" {
   zone_id = aws_route53_zone.primary.zone_id
-  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
-  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
+  name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
+  records = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
   ttl     = 60
 }
 
