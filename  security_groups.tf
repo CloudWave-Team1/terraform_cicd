d17966a3d5109f975c51045328ba3747bc294d58
malangtofu <112858761+malangtofu@users.noreply.github.com>
2023-08-16 13:21:06 +0900

    보안그룹  commit

---  security_groups.tf
+++  security_groups.tf
@@ -32,13 +32,6 @@ resource "aws_security_group" "TFC_PRD_ELB_SG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-    ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 65535
