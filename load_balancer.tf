e4c1d3d208de676a2cf3005b664a3103c09edfa9
malangtofu <112858761+malangtofu@users.noreply.github.com>
2023-08-17 22:17:42 +0900

    도메인 수정  commit

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,16 +1,16 @@
-# Route53에서 존 생성
+// Route53에서 aws.devnote.dev라는 이름의 도메인을 생성합니다.
 resource "aws_route53_zone" "aws_devnote_dev_zone" {
   name = "aws.devnote.dev"
 }
 
-# ELB에서 트래픽을 전달할 대상 그룹 생성
+// 로드 밸런서의 대상 그룹을 생성합니다. 이름은 TFC-PRD-TG이고, 포트 80에서 HTTP를 사용합니다.
 resource "aws_lb_target_group" "TFC_PRD_TG" {
   name     = "TFC-PRD-TG"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.TFC_PRD_VPC.id
 
-  # 대상 그룹의 헬스 체크 설정
+  // 대상 그룹의 상태를 확인하는 헬스 체크를 설정합니다.
   health_check {
     enabled             = true
     interval            = 30
@@ -21,12 +21,13 @@ resource "aws_lb_target_group" "TFC_PRD_TG" {
     unhealthy_threshold = 3
   }
 
+  // 대상 그룹에 이름 태그를 추가합니다.
   tags = {
     Name = "TFC-PRD-TG"
   }
 }
 
-# 웹 트래픽을 분산할 애플리케이션 로드 밸런서 생성
+// 어플리케이션 로드 밸런서를 생성합니다. 이름은 TFC-PRD-ALB입니다.
 resource "aws_lb" "TFC_PRD_ALB" {
   internal           = false
   load_balancer_type = "application"
@@ -34,65 +35,45 @@ resource "aws_lb" "TFC_PRD_ALB" {
   security_groups    = [aws_security_group.TFC_PRD_ALB_SG.id]
   subnets            = [aws_subnet.TFC_PRD_sub[0].id, aws_subnet.TFC_PRD_sub[1].id]
   enable_deletion_protection = false
+
+  // 로드 밸런서에 이름 태그를 추가합니다.
   tags = {
     Name = "TFC-PRD-ALB"
   }
 }
 
-# 생성된 ELB에 리스너 추가하고 대상 그룹 연결
+// 로드 밸런서의 HTTP 리스너를 생성합니다. 포트 80에서 동작합니다.
 resource "aws_lb_listener" "TFC_PRD_Listener_HTTP" {
   load_balancer_arn = aws_lb.TFC_PRD_ALB.arn
   port              = 80
   protocol          = "HTTP"
 
+  // 대상 그룹을 기본 동작으로 설정합니다.
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.TFC_PRD_TG.arn
   }
 }
 
+// HTTP 리스너의 리디렉션 규칙을 생성합니다.
 resource "aws_lb_listener_rule" "TFC_PRD_ListenerRule_Redirect_HTTP" {
   listener_arn = aws_lb_listener.TFC_PRD_Listener_HTTP.arn
 
-  # 리디렉션 설정
+  // 리디렉션 동작을 설정합니다.
   action {
     type = "redirect"
 
     redirect {
       host        = "#{host}"
-      path        = "/Static.html" # 리디렉션 대상 경로 설정
+      path        = "/Static.html"
       port        = "80"
       protocol    = "HTTP"
       query       = "#{query}"
-      status_code = "HTTP_301" # 301 영구 리디렉션
+      status_code = "HTTP_301"
     }
   }
 
-  # 리디렉션 조건: 루트 경로로의 요청을 리디렉트
-  condition {
-    path_pattern {
-      values = ["/"]
-    }
-  }
-}
-resource "aws_lb_listener_rule" "TFC_PRD_ListenerRule_Redirect_HTTPS" {
-  listener_arn = aws_lb_listener.TFC_PRD_Listener_HTTPS.arn
-
-  # 리디렉션 설정
-  action {
-    type = "redirect"
-
-    redirect {
-      host        = "#{host}"
-      path        = "/Static.html" # 리디렉션 대상 경로 설정
-      port        = "443"
-      protocol    = "HTTPS"
-      query       = "#{query}"
-      status_code = "HTTP_301" # 301 영구 리디렉션
-    }
-  }
-
-  # 리디렉션 조건: 루트 경로로의 요청을 리디렉트
+  // 경로 패턴 조건을 설정합니다.
   condition {
     path_pattern {
       values = ["/"]
@@ -100,13 +81,13 @@ resource "aws_lb_listener_rule" "TFC_PRD_ListenerRule_Redirect_HTTPS" {
   }
 }
 
-# HTTPS 연결을 위한 SSL/TLS 인증서 생성
+// cj.aws.devnote.dev 도메인의 ACM 인증서를 생성합니다.
 resource "aws_acm_certificate" "cert" {
   domain_name       = "cj.aws.devnote.dev"
   validation_method = "DNS"
 }
 
-# DNS 방식으로 인증서 검증을 위한 Route53 레코드 생성
+// 인증서의 DNS 검증 레코드를 생성합니다.
 resource "aws_route53_record" "cert_validation" {
   zone_id = aws_route53_zone.aws_devnote_dev_zone.zone_id
   name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
@@ -115,27 +96,28 @@ resource "aws_route53_record" "cert_validation" {
   ttl     = 60
 }
 
-# Route53 레코드를 이용하여 인증서 검증
+// ACM 인증서의 검증을 수행합니다.
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
 }
 
-# Application Load Balancer 수정: HTTPS 리스너 추가
+// 로드 밸런서의 HTTPS 리스너를 생성합니다. 포트 443에서 동작합니다.
 resource "aws_lb_listener" "TFC_PRD_Listener_HTTPS" {
   load_balancer_arn = aws_lb.TFC_PRD_ALB.arn
   port              = 443
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08" # 보안 정책 설정
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = aws_acm_certificate_validation.cert.certificate_arn
 
+  // 대상 그룹을 기본 동작으로 설정합니다.
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.TFC_PRD_TG.arn
   }
 }
 
-# ELB에 대한 A 레코드 생성
+// cj.aws.devnote.dev 도메인의 A 레코드를 생성하고 로드 밸런서의 앨리어스를 설정합니다.
 resource "aws_route53_record" "load_balancer_alias_record" {
   zone_id = aws_route53_zone.aws_devnote_dev_zone.zone_id
   name    = "cj.aws.devnote.dev"
