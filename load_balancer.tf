f024224fa22c50bf9781be3421b7173c2cd2c9fc
malangtofu <112858761+malangtofu@users.noreply.github.com>
2023-08-17 22:56:53 +0900

    로드밸런스 수정   commit

--- load_balancer.tf
+++ load_balancer.tf
@@ -56,29 +56,6 @@ resource "aws_lb_listener" "TFC_PRD_Listener_HTTP" {
 }
 
 // HTTP 리스너의 리디렉션 규칙을 생성합니다.
-resource "aws_lb_listener_rule" "TFC_PRD_ListenerRule_Redirect_HTTP_to_HTTPS" {
-  listener_arn = aws_lb_listener.TFC_PRD_Listener_HTTP.arn
-
-  // 리디렉션 동작을 설정합니다.
-  action {
-    type = "redirect"
-
-    redirect {
-      protocol    = "HTTPS"
-      port        = "443"
-      status_code = "HTTP_301"
-    }
-  }
-
-  // 모든 요청을 리디렉션 대상으로 설정합니다.
-  condition {
-    path_pattern {
-      values = ["/*"]
-    }
-  }
-}
-
-// HTTP 리스너의 리디렉션 규칙을 생성합니다.
 resource "aws_lb_listener_rule" "TFC_PRD_ListenerRule_Redirect_HTTPS" {
   listener_arn = aws_lb_listener.TFC_PRD_Listener_HTTPS.arn
 
@@ -108,6 +85,10 @@ resource "aws_lb_listener_rule" "TFC_PRD_ListenerRule_Redirect_HTTPS" {
 resource "aws_acm_certificate" "cert" {
   domain_name       = "cj.aws.devnote.dev"
   validation_method = "DNS"
+
+  subject_alternative_names = [
+    aws_lb.TFC_PRD_ALB.dns_name
+  ]
 }
 
 // 인증서의 DNS 검증 레코드를 생성합니다.
